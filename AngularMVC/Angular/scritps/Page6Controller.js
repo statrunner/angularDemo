cbdb093226498de0a0197b99924edece11a7a5a7
jason burt <jason.e.burt@gmail.com>
2018-02-20 00:11:56 -0500

    NG-Click Event demo

---
+++ AngularMVC/Angular/scritps/Page6Controller.js
@@ -0,0 +1,24 @@
+﻿var myApp = angular
+    .module("page6Module", [])
+    .controller("page6Controller", function ($scope) {
+
+        $scope.title = "Page 6";
+
+        var technologies = [
+            { name: "C#", like: 0, dislike: 0 }, 
+            { name: "Java", like: 0, dislike: 0 }, 
+            { name: "AngularJS", like: 0, dislike: 0 },
+        ];
+        
+
+        $scope.technologies = technologies;
+
+        $scope.incrementLike = function (technology) {
+            technology.like += 1;
+        }
+
+        $scope.incrementDislike = function (technology) {
+            technology.dislike += 1;
+        }
+
+    });
