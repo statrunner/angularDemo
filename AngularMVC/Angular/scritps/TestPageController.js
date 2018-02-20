533bcf71db5ef7d0210ae0c4dcc6efa018541d32
jason burt <jason.e.burt@gmail.com>
2018-02-19 22:15:25 -0500

    first model, view, controller example

---
+++ AngularMVC/Angular/scritps/TestPageController.js
@@ -0,0 +1,21 @@
+﻿/// <reference path="../../scripts/angular.min.js" />
+
+
+var myApp = angular.module("myModule", []);
+
+/*
+var myController = function ($scope) {
+    $scope.message = "AngularJS Tutorial";
+    
+}
+
+myApp.controller("myController", myController);
+*/
+
+myApp.controller("myController", function ($scope) {
+    $scope.message = "AngularJS Tutorial";
+});
+
+
+
+
