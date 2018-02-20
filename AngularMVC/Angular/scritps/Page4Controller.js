f2933893fbecd4295ff735dda937618fb2031614
jason burt <jason.e.burt@gmail.com>
2018-02-19 23:49:20 -0500

    NG-REPEAT and indexes

--- AngularMVC/Angular/scritps/Page4Controller.js
+++ AngularMVC/Angular/scritps/Page4Controller.js
@@ -2,15 +2,14 @@
     .module("page4Module", [])
     .controller("page4Controller", function ($scope) {
 
-        
-            $scope.message = "Hello Angular!";
+        $scope.title = "Employees";
 
-            var employee = {
-                firstName: "Jason",
-                lastName: "Burt",
-                age: 38
-            };
+        var employee = [
+            { firstName: "Jason", lastName: "Burt", age: 38 },
+            { firstName: "James", lastName: "Schefter", age: 31 },
+            { firstName: "Paul", lastName: "Fowler", age: 29 },
+            { firstName: "David", lastName: "Liu", age: 43 }];
 
-            $scope.employee = employee;
+        $scope.employee = employee;
 
     });
---
+++ AngularMVC/Angular/scritps/Page5Controller.js
@@ -0,0 +1,44 @@
+﻿var myApp = angular
+    .module("page5Module", [])
+    .controller("page5Controller", function ($scope) {
+
+        $scope.title = "Employees";
+
+        var employees = [
+            { firstName: "Jason", lastName: "Burt", age: 38 },
+            { firstName: "James", lastName: "Schefter", age: 31 },
+            { firstName: "Paul", lastName: "Fowler", age: 29 },
+            { firstName: "David", lastName: "Liu", age: 43 }];
+
+        $scope.employees = employees;
+
+        var countries = [
+            {
+                countryName: "UK",
+                cities: [
+                    { cityName: "London" },
+                    { cityName: "Birmingham" },
+                    { cityName: "Manchester" }
+                ]
+            },
+            {
+                countryName: "USA",
+                cities: [
+                    { cityName: "Charleston" },
+                    { cityName: "San Antonio" },
+                    { cityName: "Seattle" }
+                ]
+            },
+            {
+                countryName: "India",
+                cities: [
+                    { cityName: "Bangalore" },
+                    { cityName: "Mumbai" },
+                    { cityName: "Chennai" }
+                ]
+            }
+        ];
+
+        $scope.countries = countries;
+
+    });
